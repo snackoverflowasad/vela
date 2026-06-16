28612e7ce48165766b35b4453e3b1c1bf8f15781
Asad-bot07 <asadhussain2408@gmail.com>
2026-06-16 13:14:19 +0530

    chore: untrack compiled build directories

--- dist/walker.d.ts
+++
@@ -1,9 +0,0 @@
-import { FilterOptions } from './filters';
-export interface WalkOptions extends FilterOptions {
-}
-/**
- * Recursively yields all file paths under `rootDir`,
- * honouring ignore rules and extension filters.
- */
-export declare function walkDir(rootDir: string, opts?: WalkOptions): AsyncGenerator<string>;
-//# sourceMappingURL=walker.d.ts.map
